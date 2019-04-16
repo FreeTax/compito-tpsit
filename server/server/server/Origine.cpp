5e98c9dde376bd02f7cdb6c1d0b6f887b44772d2
francescomazzola <francesco.mazzola00@gmail.com>
2019-04-16 09:30:35 +0200

    server code realized but not tested

--- server/server/server/Origine.cpp
+++ server/server/server/Origine.cpp
@@ -1,72 +1,76 @@
-#include "pch.h"
-#include <iostream>
+#include <stdio.h>
 #include <winsock.h>
+#include <string.h>
+#include <iostream>
 #include <process.h>
-
-#pragma comment (lib, "Ws2_32.lib")
-#define STR_BUF 256
-
+#include <fstream>
+#include <string>
+#pragma comment (lib,"Ws2_32.lib")
 using namespace std;
 
-unsigned int __stdcall clientHandler(void*);
+
+SOCKET	remoteSocket;
+SOCKADDR_IN Client_addr;
+int ls_result;
+int sin_size;
+
+unsigned int __stdcall clientHandler(void*){
+		ifstream inFile;
+		inFile.open("../data.txt");
+		char buffer[3];
+		recv(remoteSocket, buffer, sizeof(buffer), 0);
+		cout << "riga richiesta : " << buffer << endl;
+		int n = stoi(buffer);
+		char ok[256] = "errore";
+		if (!inFile) {
+			cout << "Errore nell'apertura del file ";
+		}
+		else {
+			
+			for (int i = 0; i <= n; n++) {
+					inFile.getline(ok, n);
+				}
+			}
+		send(remoteSocket, ok, strlen(ok), 0);
+}
 
 int main()
 {
-	SOCKET serverSocket;
-	SOCKADDR_IN serverAddr;
+	SOCKET	listenSocket;
+	SOCKADDR_IN Server_addr;
+	short port;
+	int wsastartup;
 
 	WORD wVersionRequested = MAKEWORD(2, 2);
 	WSADATA wsaData;
-	WSAStartup(wVersionRequested, &wsaData);
+	wsastartup = WSAStartup(wVersionRequested, &wsaData);
+	if (wsastartup != NO_ERROR) cout << "Errore WSAStartup()\n" << endl;
 
-	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket < 0) cout << "Server: errore nella creazione della socket.\n" << endl;
+	else cout << "La Listening Socket è partita\n" << endl;
 
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
-	serverAddr.sin_port = htons(2700);
+	port = 27000;
 
-	bind(serverSocket, (LPSOCKADDR)&serverAddr, sizeof(struct sockaddr));
-	listen(serverSocket, SOMAXCONN);
+	Server_addr.sin_family = AF_INET;
+	Server_addr.sin_addr.s_addr = 0;
+	Server_addr.sin_port = htons(port);
 
-	printf("Server in ascolto su %s:%d\n", inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port));
+	if (bind(listenSocket, (LPSOCKADDR)&Server_addr, sizeof(struct sockaddr)) < 0)
+		cout << "Server: errore durante la bind." << endl;
 
+	ls_result = listen(listenSocket, SOMAXCONN);
+	if (ls_result < 0) printf("Server: errore durante la listen.");
+	else cout << "La Socket è in Ascolto" << endl;
 	while (true) {
-		SOCKET clientSocket = SOCKET_ERROR;
-		SOCKADDR_IN clientAddr;
-
-		while (clientSocket == SOCKET_ERROR)
-			clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &sizeof(struct sockaddr_in));
-
-		printf("Connessione stabilita con client %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-
-		_beginthreadex(0, 0, &clientHandler, (void*)clientSocket, 0, 0);
+		sin_size = sizeof(struct sockaddr_in);
+		remoteSocket = accept(listenSocket, (struct sockaddr *)&Client_addr, &sin_size);
+		cout << "client " << inet_ntoa(Client_addr.sin_addr) << "  " << ntohs(Client_addr.sin_port) << endl;
+		_beginthreadex(0, 0, &clientHandler, (void*)remoteSocket, 0, 0);
 	}
 
+	cout << "Chiudo il Server" << endl;
+	closesocket(remoteSocket);
 	WSACleanup();
-}
-
-unsigned int __stdcall clientHandler(void* data) {
-	SOCKET clientSocket = (SOCKET)data;
-
-	char buffer[STR_BUF];
-
-	for (;;) {
-
-		int res = recv(clientSocket, buffer, sizeof(buffer), 0);
-
-		if (res > 0) {
-			printf("Messaggio ricevuto: %s", buffer);
-
-			// just sends back what was received
-
-			send(clientSocket, buffer, sizeof(buffer), 0);
-		}
-		else {
-			break;
-		}
-
-	}
-
-	closesocket(clientSocket);
 	return 0;
 }
